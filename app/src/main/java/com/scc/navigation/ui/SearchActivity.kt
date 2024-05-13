af82db9d550db510597826cdc95eeaa0cef93bab
chensenchang <chensenchang1989@163.com>
2024-05-13 15:13:38 +0800

    搜索完善2

--- app/src/main/java/com/scc/navigation/ui/SearchActivity.kt
+++ app/src/main/java/com/scc/navigation/ui/SearchActivity.kt
@@ -1,47 +1,20 @@
 package com.scc.navigation.ui
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
-import android.location.Location
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.core.widget.addTextChangedListener
-import com.android.volley.Request
-import com.android.volley.RequestQueue
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.model.LatLng
+import androidx.lifecycle.Observer
 import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.AutocompletePrediction
-import com.google.android.libraries.places.api.model.AutocompleteSessionToken
-import com.google.android.libraries.places.api.model.LocationBias
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.api.model.PlaceTypes
-import com.google.android.libraries.places.api.model.RectangularBounds
-import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
-import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
 import com.google.android.libraries.places.api.net.PlacesClient
-import com.google.android.libraries.places.api.net.SearchByTextRequest
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.maps.android.SphericalUtil
-import com.scc.navigation.R
 import com.scc.navigation.base.BaseActivity
 import com.scc.navigation.data.SearchAddress
 import com.scc.navigation.databinding.ActivitySearchBinding
 import com.scc.navigation.utils.Constants
-import com.scc.navigation.utils.Constants.KEY_CURRENT_POSITION
 import com.scc.navigation.utils.Utils
 import com.scc.navigation.viewmodel.SearchViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
-import org.json.JSONArray
-import org.json.JSONException
 
 class SearchActivity :
     BaseActivity<ActivitySearchBinding, SearchViewModel>(ActivitySearchBinding::inflate) {
@@ -58,7 +31,6 @@ class SearchActivity :
 
     private val items: MutableList<SearchAddress> = ArrayList()
 
-
     override fun initListeners() {
         super.initListeners()
         binding.ivBack.setOnClickListener {
@@ -68,12 +40,15 @@ class SearchActivity :
 
     override fun onCreateFinished() {
         super.onCreateFinished()
+        viewModel.init(this@SearchActivity)
+
         binding.etSearch.addTextChangedListener(onTextChanged = { text, start, before, count ->
             //获取输入框字段并检索
             if (text?.isNotEmpty() == true) {
-                search(text.toString())
+                viewModel.queryAddressByText(text.toString())
             }
         })
+
         placesClient = Places.createClient(this)
         initRecyclerView()
 
@@ -93,25 +68,10 @@ class SearchActivity :
             setResult(Activity.RESULT_OK, data)
             finish()
         }
-    }
 
-    @SuppressLint("MissingPermission")
-    private fun search(key: String) {
-        // 使用字段定义要返回的数据类型.
-        val placeFields =
-            listOf(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.ID, Place.Field.LAT_LNG)
-        val textRequest = SearchByTextRequest.builder(key, placeFields).build();
-
-        placesClient.searchByText(textRequest)
-            .addOnSuccessListener {
-                it.places.forEach { it ->
-                    items.add(SearchAddress(it.name, it.address, it.id, it.latLng))
-                }
-                searchAdapter.setAddress(items);
-            }
-            .addOnFailureListener {
-                searchAdapter.setAddress(emptyList())
-            }
+        viewModel.liveData.observe(this@SearchActivity, Observer {
+            searchAdapter.setAddress(items);
+        })
     }
 
 }
--- app/src/main/java/com/scc/navigation/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/scc/navigation/viewmodel/SearchViewModel.kt
@@ -1,12 +1,49 @@
 package com.scc.navigation.viewmodel
 
+import android.app.Activity
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.api.net.SearchByTextRequest
 import com.scc.navigation.base.BaseViewModel
 import com.scc.navigation.data.SearchAddress
+import com.scc.navigation.ui.SearchActivity
 
-class SearchViewModel : BaseViewModel() {
+class SearchViewModel() : BaseViewModel() {
 
-    val  items = MutableLiveData<SearchAddress>();
+    val liveData = MutableLiveData<List<SearchAddress>>()
+
+    private lateinit var placesClient: PlacesClient
+
+    fun init(activity: SearchActivity){
+        placesClient =Places.createClient(activity)
+    }
+
+    /**
+     * 根据关键字查找
+     */
+    fun queryAddressByText(text: String) {
+        val items = mutableListOf<SearchAddress>()
+
+        // 使用字段定义要返回的数据类型.
+        val placeFields =
+            listOf(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.ID, Place.Field.LAT_LNG)
+        val textRequest = SearchByTextRequest.builder(text, placeFields).build();
+        showLoading()
+        placesClient.searchByText(textRequest)
+            .addOnSuccessListener {
+                it.places.forEach { it ->
+                    items.add(SearchAddress(it.name, it.address, it.id, it.latLng))
+                }
+                liveData.value = items;
+                hideLoading()
+            }
+            .addOnFailureListener {
+                liveData.value = items;
+                hideLoading()
+            }
+    }
 
 }
